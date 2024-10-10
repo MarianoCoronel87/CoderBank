96efe1ba465a1632f585457d5c932b6c56a6c20f
Mariano Coronel <marianocoronel947@gmail.com>
2024-10-10 13:56:14 -0300

    coderBank

--- js/main.js
+++ js/main.js
@@ -144,3 +144,78 @@ function actualizarUsuarioActual(usuario) {
     localStorage.setItem('usuarioActual', JSON.stringify(usuario));  
     localStorage.setItem(usuario.usuario, JSON.stringify(usuario));  
 }
+document.addEventListener("DOMContentLoaded", () => {  
+    const saldoElemento = document.getElementById("saldo");  
+    const depositarBoton = document.getElementById("depositarBoton");  
+    const retirarBoton = document.getElementById("retirarBoton");  
+    const movimientosContenedor = document.getElementById("contenedor-movimientos");  
+    const prestamoBoton = document.getElementById("prestamoBoton");  
+    const eyeIcon = document.getElementById("eye-icon");  
+    const userJSON = localStorage.getItem('usuarioActual');  
+  
+    if (!userJSON) {  
+      Swal.fire('Error', 'No has iniciado sesión.', 'error').then(() => {  
+        window.location.href = "../login.html";  
+      });  
+      return;  
+    }  
+  
+    const userObj = JSON.parse(userJSON);  
+    const cuenta = new CuentaBancaria();  
+    cuenta.saldo = userObj.cuentaBancaria.saldo;  
+    cuenta.movimientos = userObj.cuentaBancaria.movimientos;  
+    const usuarioActual = new Usuario(userObj.usuario, userObj.contraseña);  
+    usuarioActual.cuentaBancaria = cuenta;  
+    saldoElemento.textContent = `$${usuarioActual.cuentaBancaria.saldo}`;  
+  
+    registrarMovimientoEnPantalla(usuarioActual);  
+    obtenerCotizacionesDolarBlue();
+    // Event Listeners  
+    depositarBoton.addEventListener("click", () => depositarSaldo(usuarioActual));  
+    retirarBoton.addEventListener("click", () => retirarSaldo(usuarioActual));  
+    prestamoBoton.addEventListener("click", () => pedirPrestamo(usuarioActual));  
+    eyeIcon.addEventListener("click", () => ocultarSaldo(usuarioActual));  
+  });  
+  function obtenerCotizacionesDolarBlue() {  
+  
+    const url = "https://dolarapi.com/v1/dolares/blue";  
+  
+    fetch(url)  
+      .then(response => {  
+        if (!response.ok) {  
+          throw new Error('Error en la respuesta de la API');  
+        }  
+        return response.json();  
+      })  
+      .then(data => {  
+        const compra = data.compra;  
+        const venta = data.venta;  
+        document.getElementById('compraBlue').textContent = `$${compra}`;  
+        document.getElementById('ventaBlue').textContent = `$${venta}`;  
+      })  
+      .catch(error => {  
+        console.error(error);  
+        Swal.fire({  
+          title: 'Algo salió mal...',  
+          html: '<p class="swaltext">Error al cargar los datos</p>',  
+          icon: 'error',  
+          showConfirmButton: true,  
+          confirmButtonText: 'Ok',  
+          confirmButtonColor: '#273746',  
+          timer: 5000,  
+          timerProgressBar: true,  
+          allowOutsideClick: false,  
+          allowEscapeKey: true,  
+          allowEnterKey: true,   
+          background: '#fff',  
+          customClass: {  
+            title: 'swaltitle',  
+          }  
+        });  
+      });  
+  }
+  function formatearNumero(numero) {  
+    return new Intl.NumberFormat('es-AR', { style: 'currency', currency: 'ARS' }).format(numero);  
+  }  
+  document.getElementById('compraBlue').textContent = `${formatearNumero(compra)}`;  
+  document.getElementById('ventaBlue').textContent = `${formatearNumero(venta)}`;    
